ed5f5826b5ccf7e4912cfe11177a2a986085e4b8
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-05-15 19:33:42 -0400

    question generator function

--- js/script.js
+++ js/script.js
@@ -4,23 +4,27 @@ var startBtnEl = document.querySelector("#startQuiz");
 var questionBtnEl = document.querySelector("#questionButtons");
 var footerEl = document.querySelector(".card-footer");
 
+var btn1 = questionBtnEl.children[0];
+var btn2 = questionBtnEl.children[1];
+var btn3 = questionBtnEl.children[2];
+
 var score = 0
 var questionNum = 0
 var questions = [
-    {q1: "Dwight owns and runs a farm in his spare time. What does his farm primarily produce?",
-        a: ["Beets", "Apples", "Potatoes"]},
+    {1: "Dwight owns and runs a farm in his spare time. What does his farm primarily produce?",
+        a: ["Apples", "Potatoes", "Beets"]},
  
-    {q2: "What does Micheal burn his foot on?",
+    {2: "What does Micheal burn his foot on?",
         a: ["A George Foreman Grill", "The fire Ryan started", "Kevin's homemade chili"]},
 
-    {q3: "Who are the three main members of the party planning committee",
-        a: ["Phyllis, Angela, and Pam", "Angela, Phyllis, and Kelly", "Phyllis, Kelly, and Pam"]},
+    {3: "Who are the three main members of the party planning committee",
+        a: ["Angela, Phyllis, and Kelly", "Phyllis, Angela, and Pam", "Phyllis, Kelly, and Pam"]},
 
-    {q4: "What is Andy's nickname for Jim?", 
-        a: ["Big Tuna", "Small Flounder", "Medium Swordfish"]},
+    {4: "What is Andy's nickname for Jim?", 
+        a: ["Small Flounder", "Medium Swordfish", "Big Tuna"]},
 
-    {q5: "What is Phyllis' husbands name?",
-        a: ["Bob Vance, Vance Refridgeration", "Kevin Malone", "Bob Ross"]}
+    {5: "What is Phyllis' husbands name?",
+        a: ["Kevin Malone", "Bob Ross", "Bob Vance, Vance Refridgeration"]}
 ]
 
 // When user clicks "start quiz" button
@@ -34,10 +38,18 @@ startBtnEl.addEventListener("click", function() {
 // Sets new display for question
 function question() {
     titleEl.textContent = ("Question # " + questionNum);
+    GenerateQuestion();
 
 }
 
-//function GenerateQuestion() {
-    //for (i = 0; i < questions.length; i++)
+function GenerateQuestion() {
+    var index = questionNum -1
+    cardTextEl.textContent = (questions[index][1]);
+    questionNum++;
 
+    btn1.textContent = questions[index].a[0];
+    btn2.textContent = questions[index].a[1];
+    btn3.textContent = questions[index].a[2];
+
+}
 
